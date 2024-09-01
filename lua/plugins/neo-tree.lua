21c6e8e84ef7629d6588103140f1baf16f33b2f3
Techi-Freki <techifreki@proton.me>
2024-08-31 23:34:41 -0400

    added neotree file nesting config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -4,7 +4,31 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim"
+		"MunifTanjim/nui.nvim",
+		"saifulapm/neotree-file-nesting-config",
 	},
 	cmd = "Neotree",
+	opts = {
+		hide_root_node = true,
+		retain_hidden_root_indent = true,
+		filesystem = {
+			filtered_items = {
+				show_hidden_count = false,
+				never_show = {
+					".DS_Store",
+				},
+			},
+		},
+		default_component_configs = {
+			indent = {
+				with_expanders = true,
+				expander_collapsed = "",
+				expander_expanded = "",
+			},
+		},
+	},
+	config = function(_, opts)
+		opts.nesting_rules = require("neotree-file-nesting-config").nesting_rules
+		require("neo-tree").setup(opts)
+	end,
 }
