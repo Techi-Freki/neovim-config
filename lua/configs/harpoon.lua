2060c805a130bcdfe32475003c8ac5d931211ed6
Techi-Freki <techifreki@proton.me>
2024-11-16 15:57:03 -0500

    fixing harpoon config

--- lua/configs/harpoon.lua
+++ lua/configs/harpoon.lua
@@ -20,14 +20,14 @@ local function toggle_telescope(harpoon_files)
 		}):find()
 end
 
-map("n", "<leader>ha", function() harpoon:list():add() end)
-map("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+map("n", "<leader>ha", function() harpoon:list():add() end, { desc = "Adds a buffer to the harpoon list" })
+map("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Toggles the harpoon list" })
 
-map("n", "<leader>h1", function() harpoon:list():select(1) end)
-map("n", "<leader>h2", function() harpoon:list():select(2) end)
-map("n", "<leader>h3", function() harpoon:list():select(2) end)
-map("n", "<leader>h4", function() harpoon:list():select(4) end)
-map("n", "<leader>h5", function() harpoon:list():select(5) end)
+map("n", "<leader>h1", function() harpoon:list():select(1) end, { desc = "Opens the first item in the harpoon list" })
+map("n", "<leader>h2", function() harpoon:list():select(2) end, { desc = "Opens the 2nd item in the harpoon list" })
+map("n", "<leader>h3", function() harpoon:list():select(2) end, { desc = "Opens the 3rd item in the harpoon list" })
+map("n", "<leader>h4", function() harpoon:list():select(4) end, { desc = "Opens the 4th item in the harpoon list" })
+map("n", "<leader>h5", function() harpoon:list():select(5) end, { desc = "Opens the 5th item in the harpoon list" })
 
-map("n", "<leader>hp", function() harpoon:list():prev() end)
-map("n", "<leader>hn", function() harpoon:list():next() end)
+map("n", "<leader>hp", function() harpoon:list():prev() end, { desc = "Navigates to the previous buffer in the harpoon list" })
+map("n", "<leader>hn", function() harpoon:list():next() end, { desc = "Navigates to the next buffer in the harpoon list" })
